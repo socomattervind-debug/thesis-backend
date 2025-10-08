1cf3e28a702f669a59184791383db31ad02f7e84
Railey Pacheco <111040098+Railey29@users.noreply.github.com>
2025-10-07 21:35:11 -0800

    Update

--- routes/fireFighterAddRecords.js
+++ routes/fireFighterAddRecords.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const FireIncident = require("../models/FireIncident");
+const FireIncident = require("../models/fireIncident");
 
 // ===============================
 // 🧾 ADD NEW FIRE INCIDENT RECORD
